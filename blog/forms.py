39d1540c4003338c28d6ba916ce99b2e6dde0eb5
jsj0718 <jeongsejin718@gmail.com>
2020-12-01 23:47:34 +0900

    Add to draft and remove

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Post
 
+# PostForm이라는 ModelForm을 선언
 class PostForm(forms.ModelForm):
     
+    # 이 Form을 만들기 위해 어떤 Model이 쓰이는지 Django에 알려주는 구문
     class Meta:
         model = Post
         fields = ('title', 'text', )
--- blog/views.py
+++ blog/views.py
@@ -11,32 +11,59 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
+    # pk에 해당하는 Post가 없는 경우, 404 Error Page를 띄움.
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
 
 def post_new(request):
+    # 전송 방식이 POST인 경우
     if request.method == "POST":
+        # Form에서 받은 데이터를 PostForm에 전송
         form = PostForm(request.POST)
+        # 값이 유효할 때
         if form.is_valid():
-            post = form.save(commit=False)
+            # Form을 저장하는 작업
+            post = form.save(commit=False)      # commit=False : 넘겨진 데이터를 바로 Post모델에 저장하지 말라는 의미
+            # 작성자를 추가하는 작업
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
+            # post.pk를 이용해 View(post_detail)에게 전송, render와 redirect 차이 알아보기 
             return redirect('post_detail', pk = post.pk)
+    # 전송 방식이 그 외인 경우 (e.x. GET)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form' : form})
 
 def post_edit(request, pk):
+    # URL로부터 pk 매개변수를 받아서 처리
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
+        # 수정하고자 하는 글의 Post모델 instance로 가져온다. (pk로 원하는 글을 찾는다.)
+        # 폼을 만들 때(이전에 입력한 데이터 값)와 저장할 때 사용하게 된다.
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    # published_date_isnull=True로 발행되지 않은 글 목록을 가져온다.
+    # order_by('created_date')로 필드에 대해 오름차순 정렬을 수행한다.
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts' : posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
